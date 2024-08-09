126570e140c44fbdebf1b563355846774d074173
MahabirGupta <mahabirg81@gmail.com>
2024-08-09 10:57:38 +0800

    Use add() method to add the elements

--- LinkHashSetConcept.java
+++ LinkHashSetConcept.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 public class LinkHashSetConcept {
     public static void main(String[] args) {
-        System.out.println("Hello");
+//        Declare a LinkedHashSet
+
+        LinkedHashSet linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("Hello");
+        linkedHashSet.add(32);
+        linkedHashSet.add(45.67);
+        linkedHashSet.add(false);
+        linkedHashSet.add(null);
+
+        System.out.println(linkedHashSet); // insertion order is preserved
+
+//        declare a HashSet
+        HashSet hashSet = new HashSet<>();
+        hashSet.add("Hello");
+        hashSet.add(32);
+        hashSet.add(45.67);
+        hashSet.add(false);
+        hashSet.add(null);
+
+        System.out.println(hashSet); // insertion order is not preserved
     }
 }
